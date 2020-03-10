973583f15f9eae2368df61b19878376a6a25a997
happydelly <1276443804@qq.com>
2020-03-10 10:48:21 +0800

    test

--- src/main/java/com/imooc/mall/controller/CartController.java
+++ src/main/java/com/imooc/mall/controller/CartController.java
@@ -20,7 +20,7 @@ public class CartController {
     @Autowired
     private ICartService cartService;
 
-    @PostMapping(value = "/carts/add")
+    @PostMapping(value = "/carts/adddev")
     public ResponseVo<CartVo> add(@Valid @RequestBody CartAddForm cartAddForm, HttpSession session){
         User user = (User) session.getAttribute(MallConst.CURRENT_USER);
         return cartService.add(user.getId(),cartAddForm);
